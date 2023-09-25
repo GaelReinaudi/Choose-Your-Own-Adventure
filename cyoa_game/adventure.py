1a1eb3af517c65b63342a14d7aaa2ccfdbd69e17
Maya-Hacker <maya.rm.nyc@gmail.com>
2023-09-25 18:59:05 -0400

    It works much better when using langchain rather than the "llm" library

--- cyoa_game/adventure.py
+++ cyoa_game/adventure.py
@@ -1,33 +1,40 @@
 import asyncio
-import llm
+
+# import llm
 import textwrap
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.chat_models import ChatOpenAI
 
 
 class Adventure:
     def __init__(self, api_key):
-        self.model = llm.get_model("gpt-3.5-turbo-16k")
-        self.model.key = api_key
+        # self.model = llm.get_model("gpt-3.5-turbo-16k")
+        # self.model.key = api_key
+        self.model = ChatOpenAI(model="gpt-3.5-turbo-16k", openai_api_key=api_key)
 
     def inital_prompt(self) -> str:
-        return """
+        messages = [
+            SystemMessage(
+                content="You are a talented author of Choose-Your-Own-Adventure books"
+            ),
+            HumanMessage(
+                content="""
 Write the first introductory part of a book of the type "chose your own adventure" \
 where the reader, which is the main character, is a girl coder. the introductory part \
 is typically 3 pages long.
 Remarque: do not mention the page numbers, just write it in one \
 block of a dozen paragraphs.
 """
+            ),
+        ]
+        return messages
 
     def format_text(self, input_text, width=50):
         return textwrap.fill(input_text, width=width)
 
     async def start(self):
-        response = self.model.prompt(
-            self.inital_prompt(),
-            max_tokens=4000,
-            stop="\n\n",
-            system="You are a talented author of Choose-Your-Own-Adventure books",
-        )
-        formatted_text = self.format_text(response.text())
+        response = self.model(self.inital_prompt())
+        formatted_text = self.format_text(response.content)
         print(formatted_text)
         key = input("Make your choice:")
 
