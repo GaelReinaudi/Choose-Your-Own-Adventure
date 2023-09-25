0fa15c10e881144f84194f390b6b387c06a49eae
Maya-Hacker <maya.rm.nyc@gmail.com>
2023-09-25 18:30:29 -0400

    trying better starting prompt

--- cyoa_game/adventure.py
+++ cyoa_game/adventure.py
@@ -5,21 +5,28 @@ import textwrap
 
 class Adventure:
     def __init__(self, api_key):
-        self.model = llm.get_model("gpt-3.5-turbo")
+        self.model = llm.get_model("gpt-3.5-turbo-16k")
         self.model.key = api_key
 
     def inital_prompt(self) -> str:
         return """
-write the first paragraphs of a book of the type "chose your own adventure" \
-where the reader, which is the main character, is a girl coder that discovers \
-something very strange while coding on her linux machine...
+Write the first introductory part of a book of the type "chose your own adventure" \
+where the reader, which is the main character, is a girl coder. the introductory part \
+is typically 3 pages long.
+Remarque: do not mention the page numbers, just write it in one \
+block of a dozen paragraphs.
 """
 
     def format_text(self, input_text, width=50):
         return textwrap.fill(input_text, width=width)
 
     async def start(self):
-        response = self.model.prompt(self.inital_prompt(), max_tokens=1000, stop="\n\n")
+        response = self.model.prompt(
+            self.inital_prompt(),
+            max_tokens=4000,
+            stop="\n\n",
+            system="You are a talented author of Choose-Your-Own-Adventure books",
+        )
         formatted_text = self.format_text(response.text())
         print(formatted_text)
         key = input("Make your choice:")
